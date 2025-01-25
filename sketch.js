e87447d7a44b65b7c7a5e5105592ccec65e156f8
DC75166 <daipayanchakraborty75@gmail.com>
2025-01-25 08:29:31 +0530

    Created a master function to write all code inside
    Created a master sketch function to encapsulate
    all logic and included the main grid function
    inside it. Created additional two div containers
    in the initial container div.

--- sketch.js
+++ sketch.js
@@ -1,18 +1,28 @@
-function makeGrid(size) {
+function etchSketch() {
     const container = document.querySelector("#container");
+    const sketchContainer = document.createElement("div");
+    sketchContainer.classList.add("sketch-container");
+    const sketchScreen  = document.createElement("div");
+    sketchScreen.classList.add("sketch-screen");
+    container.appendChild(sketchContainer);
+    container.appendChild(sketchScreen);
 
+    // Function to create the grid boxes
+  function makeGrid(size) {
     for (let i = 0; i < size; i++) {
-        const row = document.createElement("div")
-        row.classList.add("row");
-        container.appendChild(row);
-        for (let j = 1; j <= size; j++) {
-            const column = document.createElement("div");
-            column.classList.add("column");
-            column.textContent = "This is a test";
-            column.style.border = "1px solid red";
-            column.innerText = (i * size) + j;
-            container.appendChild(column);
-        }
+      const row = document.createElement("div");
+      row.classList.add("row");
+      container.appendChild(row);
+      for (let j = 1; j <= size; j++) {
+        const column = document.createElement("div");
+        column.classList.add("column");
+        column.textContent = "This is a test";
+        column.style.border = "1px solid red";
+        column.innerText = i * size + j;
+        container.appendChild(column);
+      }
     }
+  }
+  makeGrid(16);
 }
-makeGrid(16);
+etchSketch();
