ea73a4d64eb8045a71678fd761c6d89f73fca2bd
DC75166 <daipayanchakraborty75@gmail.com>
2025-01-25 10:54:20 +0530

    Grid created and style added
    The grid is created using flexbox and border and
    heading added

--- sketch.js
+++ sketch.js
@@ -1,26 +1,29 @@
 function etchSketch() {
-    const container = document.querySelector("#container");
-    const sketchContainer = document.createElement("div");
-    sketchContainer.classList.add("sketch-container");
-    const sketchScreen  = document.createElement("div");
-    sketchScreen.classList.add("sketch-screen");
-    container.appendChild(sketchContainer);
-    container.appendChild(sketchScreen);
+  const container = document.querySelector("#container");
+  const sketchContainer = document.createElement("div");
+  sketchContainer.classList.add("sketch-container");
+  const sketchScreen = document.createElement("div");
+  sketchScreen.classList.add("sketch-screen");
+  container.appendChild(sketchContainer);
+  container.appendChild(sketchScreen);
+  const heading = document.createElement("h1");
+  heading.textContent = "Etch-A-Sketch";
+  sketchContainer.appendChild(heading);
 
-    // Function to create the grid boxes
+  // Function to create the grid boxes
   function makeGrid(size) {
     for (let i = 0; i < size; i++) {
       const row = document.createElement("div");
       row.classList.add("row");
-      container.appendChild(row);
       for (let j = 1; j <= size; j++) {
         const column = document.createElement("div");
         column.classList.add("column");
         column.textContent = "This is a test";
         column.style.border = "1px solid red";
         column.innerText = i * size + j;
-        container.appendChild(column);
+        row.appendChild(column);
       }
+      sketchScreen.appendChild(row);
     }
   }
   makeGrid(16);
