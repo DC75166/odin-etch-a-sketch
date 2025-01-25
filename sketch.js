3f4deb9a67959575af6a430e15c4c644131d1779
DC75166 <daipayanchakraborty75@gmail.com>
2025-01-25 18:11:15 +0530

    Included the buttons in a seperate grid to style
    Click to play starts the game. Clear grid clears
    and resets the game. Included them in a seperate
    div to style them better.

--- sketch.js
+++ sketch.js
@@ -10,23 +10,23 @@ function etchSketch() {
   const heading = document.createElement("h1");
   heading.textContent = "Etch-A-Sketch";
   sketchContainer.appendChild(heading);
-  button = document.createElement("button");
-  button.textContent = "Click to play";
-  heading.appendChild(button);
+  const buttonDiv = document.createElement("div");
+  buttonDiv.classList.add("button-div");
+  heading.appendChild(buttonDiv);
 
   // Function to create the grid boxes
   function makeGrid(size) {
-      for (let i = 0; i < size; i++) {
-        const column = document.createElement("div");
-        column.classList.add("column");
-        for (let j = 1; j <= size; j++) {
-          const row = document.createElement("div");
-          row.classList.add("row");
-          column.appendChild(row);
-        }
-        sketchScreen.appendChild(column);
+    for (let i = 0; i < size; i++) {
+      const column = document.createElement("div");
+      column.classList.add("column");
+      for (let j = 1; j <= size; j++) {
+        const row = document.createElement("div");
+        row.classList.add("row");
+        column.appendChild(row);
       }
+      sketchScreen.appendChild(column);
     }
+  }
 
   // Function to change the color of the grid boxes
   function changeColor() {
@@ -36,32 +36,50 @@ function etchSketch() {
         const randomR = Math.floor(Math.random() * 256);
         const randomG = Math.floor(Math.random() * 256);
         const randomB = Math.floor(Math.random() * 256);
-        const random =  Math.random();
-        row.setAttribute("style", `background-color: rgba(${randomR},${randomG},${randomB},${random}`);
+        const random = Math.random();
+        row.setAttribute(
+          "style",
+          `background-color: rgba(${randomR},${randomG},${randomB},${random}`
+        );
       });
     });
   }
-  
+
   // Function to initiate the game
-  function playGame(){
-     button.addEventListener("click",()=>{
+  function playGame() {
+    button = document.createElement("button");
+    button.textContent = "Click to play";
+    buttonDiv.appendChild(button);
+    button.addEventListener("click", () => {
       const size = parseInt(window.prompt("Enter the size of the grid"));
-      if(!isNaN(size) && (size>0 && size<=100)){
+      if (!isNaN(size) && size > 0 && size <= 100) {
         sketchScreen.textContent = "";
         makeGrid(size);
-      }
-      else if (size<=0 || size>100){
-        alert("The size of the grid should be greater than 0 and less than or equal to 100");
+        changeColor();
+      } else if (size <= 0 || size > 100) {
+        alert(
+          "The size of the grid should be greater than 0 and less than or equal to 100"
+        );
         sketchScreen.textContent = "Click the button to Refresh";
-      }
-      else{
+      } else {
         alert("Please enter a valid number");
         sketchScreen.textContent = "Click the button to Refresh";
       }
-      changeColor();
-     })
+    });
+  }
+
+  //Function to reset the grid
+  function clearGrid() {
+    const clearButton = document.createElement("button");
+    clearButton.textContent = "Clear Grid";
+    buttonDiv.appendChild(clearButton);
+    clearButton.addEventListener("click", () => {
+      sketchScreen.setAttribute("style", "background-color: white");
+      sketchScreen.textContent = "";
+    });
   }
 
   playGame();
+  clearGrid();
 }
 etchSketch();
